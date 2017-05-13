fb6311e6798495109ff9c87f5626f9d06e47a1a6
Ekaterina Vaartis <TyanNN@cocaine.ninja>
2017-05-13 19:17:54 +0300

    FncCall test with parameter

--- test/ASTParser/FncCall.cpp
+++ test/ASTParser/FncCall.cpp
@@ -23,3 +23,31 @@ TEST(FncCall, Simple) {
 
     ASSERT_EQ(call->name, "test");
 }
+
+TEST(FncCall, WithParam) {
+    ASTParser par("fnc test(int x) int { ret x; } fnc main() { test(10); }");
+
+    vector<FncDefAST> fns = par.functions;
+    ASSERT_EQ(fns.size(), 2);
+
+    FncDefAST testf = fns[0];
+    FncDefAST mainf = fns[1];
+
+    ASSERT_EQ(mainf.body.size(), 1);
+
+    ASSERT_EQ(testf.body.size(), 1);
+    ASSERT_EQ(testf.args.size(), 1);
+
+    shared_ptr<BaseAST> ca = mainf.body[0];
+
+    FncCallAST *call = dynamic_cast<FncCallAST *>(ca.get());
+
+    ASSERT_NE(call, nullptr);
+
+    ASSERT_EQ(call->name, "test");
+
+    Expression *e = dynamic_cast<Expression *>(call);
+    ASSERT_NE(e, nullptr);
+
+    ASSERT_EQ(e->expression_type, testf.ret_type);
+}
