c50b10a8a28246303ed23788acdfe83763af0a91
Ekaterina Vaartis <TyanNN@cocaine.ninja>
2017-05-13 20:07:15 +0300

    Implement tests

---
+++ test/ASTParser/Implement.cpp
@@ -0,0 +1,35 @@
+#include "ASTParser.hpp"
+#include "gtest/gtest.h"
+
+TEST(Implement, Empty) {
+    ASTParser par("type T { int x } implement for T { }");
+
+    vector<ImplementAST> impls = par.impls;
+
+    ASSERT_EQ(impls.size(), 1);
+
+    ImplementAST impl = impls[0];
+
+    ASSERT_EQ(impl.type, "T");
+    ASSERT_EQ(impl.fncs.size(), 0);
+}
+
+TEST(Implement, Simple) {
+    ASTParser par("type T { int x } implement for T { fnc f() int { ret self.x; } }");
+
+    vector<ImplementAST> impls = par.impls;
+
+    ASSERT_EQ(impls.size(), 1);
+
+    ImplementAST impl = impls[0];
+
+    ASSERT_EQ(impl.type, "T");
+    ASSERT_EQ(impl.fncs.size(), 1);
+
+    FncDefAST f = impl.fncs[0];
+    ASSERT_EQ(f.body.size(), 1);
+}
+
+TEST(Implement, NonexistantType) {
+    ASSERT_DEATH(ASTParser par("implement for T { fnc f() int { ret self.x; }"), "ImplementAST.*Assertion.*failed");
+}
