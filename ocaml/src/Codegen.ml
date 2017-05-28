ea536893919286e3382cb7425ae88bbf7cb4f9cb
Ekaterina Vaartis <TyanNN@cocaine.ninja>
2017-05-28 21:40:45 +0300

    Nope, no coverage

--- ocaml/src/Codegen.ml
+++ ocaml/src/Codegen.ml
@@ -1,6 +1,10 @@
 open Llvm;;
 open AST;;
 
+module BuiltFnc = struct
+  type t = { fnc: llvalue; ret_value: llvalue option; ret_block: llbasicblock };;
+end;;
+
 let codegen ast =
   let context = global_context () in
   let modl = create_module context "stdin" in
@@ -8,6 +12,7 @@ let codegen ast =
 
   let functions = Hashtbl.create 1 in
   let curr_variables = Hashtbl.create 0 in
+  let curr_fn_name = ref "" in
 
   let get_llvm_type tp =
     match tp with
@@ -63,12 +68,25 @@ let codegen ast =
         let open AST.Decl in
         let alloca = build_alloca (get_llvm_type x.tp) x.name builder in
         begin match x.value with
-        | Some v -> ignore(build_store alloca (gen_expr v) builder)
+        | Some v -> ignore(build_store (gen_expr v) alloca builder)
         | None -> ()
         end;
         Hashtbl.replace curr_variables x.name alloca;
       end
     | ExprAsStmt x -> ignore(gen_expr x)
+    | Ret x -> begin
+        let f = Hashtbl.find functions !curr_fn_name in
+        begin
+          match x.value with
+          | None -> ()
+          | Some va ->
+             let ret_v = match f.BuiltFnc.ret_value with
+               | Some v -> v
+               | None -> failwith "Can't return value from void function" in
+             ignore(build_store (gen_expr va) ret_v builder)
+        end;
+        ignore(build_br f.BuiltFnc.ret_block builder)
+      end
   in
 
   List.iter (fun node ->
@@ -76,12 +94,26 @@ let codegen ast =
       | FncDef x | OperatorDef x -> begin
           let open AST.FncDef in
           let m_name = if x.name <> "main" then mangle node else x.name in
+          curr_fn_name := m_name;
           let ftype = function_type (get_llvm_type x.FncDef.ret_t) (Array.of_list @@ List.map (fun (x,y) -> get_llvm_type y) x.args) in
-          let fn = define_function m_name ftype modl in
-          position_at_end (entry_block fn) builder;
+          let fnc = define_function m_name ftype modl in
+          position_at_end (entry_block fnc) builder;
 
+          let ret_block = append_block context "exit" fnc in
+          let ret_value = match x.FncDef.ret_t with
+            | Void -> position_at_end ret_block builder;
+                      build_ret_void builder;
+                      None
+            | va ->
+               let r = build_alloca (get_llvm_type va) "ret_val" builder in
+               position_at_end ret_block builder;
+               ignore(build_ret (build_load r "" builder) builder);
+               Some r in
+          position_at_end (entry_block fnc) builder;
+
+          position_at_end (entry_block fnc) builder;
+
+          Hashtbl.replace functions m_name { BuiltFnc.fnc; ret_value; ret_block };
           List.iter gen_stmt x.body;
-
-          Hashtbl.replace functions m_name fn;
         end
     ) ast; modl;;
